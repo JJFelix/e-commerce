1cd3db42e5c55abcd4124c9ff5e6d6763836fa58
MiniThought17 <felixomwenga441@gmail.com>
2023-08-01 22:32:46 +0300

    Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative">
+                <img src="images/main-banner-1.jpg"className='img-fluid rounded-2' alt="main-banner" />
+                <div className="main-banner-content position-absolute">
+                  <h4>Supercharged for Pros</h4>
+                  <h5>iPad S13+ Pro</h5>
+                  <p>From $999.00 or $41.62/mo</p>
+                  <Link className='button'>BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-6">
+              <div className="d-flex flex-wrap justify-content-between align-items-center">
+                <div className="small-banner position-relative mb-2">
+                  <img src="images/catbanner-01.jpg"className='img-fluid rounded-2' alt="main-banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Best sale</h4>
+                    <h5>iPad S13+ Pro</h5>
+                    <p>From $999.00 <br />  $41.62/mo</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative mb-2">
+                  <img src="images/catbanner-02.jpg"className='img-fluid rounded-2' alt="main-banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Supercharged for Pros</h4>
+                    <h5>iPad S13+ Pro</h5>
+                    <p>From $999.00 <br />  $41.62/mo</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative mb-2">
+                  <img src="images/catbanner-03.jpg"className='img-fluid rounded-2' alt="main-banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>new arrival</h4>
+                    <h5>Buy iPad Air</h5>
+                    <p>From $999.00 <br />  $41.62/mo</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative mb-2">
+                  <img src="images/catbanner-04.jpg"className='img-fluid rounded-2' alt="main-banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>new arrival</h4>
+                    <h5>Buy iPad Air</h5>
+                    <p>From $999.00 <br />  $41.62/mo</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
